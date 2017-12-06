068b66eab287bfb314c4d6b0f3c8f9d45f154484
haagen17 <33736132+haagen17@users.noreply.github.com>
2017-12-06 18:24:05 -0500

    Add files via upload

--- auctiontest.js
+++ auctiontest.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   var sellerName;
   var itemName;
   var itemPrice;
@@ -8,65 +7,29 @@ $(document).ready(function() {
   var itemExp;
   var itemDesc;
   var submitform = [sellerName, itemName, itemDesc, itemPrice, itemCat, itemExp];
+  /*
+    $("input").click(function() {
+      if ($("tr#" + $(this).data("itemDesc")).is(":visible")) {
+        $("tr#" + $(this).data("itemDesc")).remove();
+      } else {
+        $(this).closest("tr").after('<tr id="' + $(this).data("itemDesc") + '"><td colspan="7">' +
+          $('#' + $(this).data("itemDesc")).html() + '</td></tr>');
+      }
+    });*/
 
-  $("input").on("click", function() {
-    revealText(this);
+
+  $("#sellersubmit").click(function() {
+
+    var sellerName = $("#sellername").val();
+    var itemName = $("#itemname").val();
+    var itemDesc = $("#itemdescription").val();
+    var itemPrice = $("#startingprice").val();
+    var itemCat = $("#itemcategory").val();
+    var itemExp = $("#expirationdate").val();
+
+    var markup = "<tr><td><input type='button' name='' value='Item Description' data-href='content2'></td><td>" + itemName + "</td><td>" + sellerName + "</td><td>" + itemCat + "</td><td>" + itemPrice + "</td><td>" + itemExp + "</td></tr>";
+    $("#itemsTable").append(markup);
   });
 
-  function revealText(this) {
-    if ($("tr#" + $(this).data("itemDesc")).is(":visible")) {
-      $("tr#" + $(this).data("itemDesc")).remove();
-    } else {
-      $(this).closest("tr").after('<tr id="' + $(this).data("itemDesc") + '"><td colspan="7">' +
-        $('#' + $(this).data("itemDesc")).html() + '</td></tr>');
-    }
-  }
-
-  $("#sellersubmit").on("click", function() {
-    alert("submittted");
-    submitItem(this);
-    itemID++;
-  });
-
-  function submitItem(this) {
-    sellerName = $("#sellername").val();
-    itemName = $("#itemname").val();
-    itemDesc = $("#itemdescription").val();
-    itemPrice = $("#startingprice").val();
-    itemCat = $("#itemcategory").val();
-    itemExp = $("#expirationdate").val();
-    var $newtr1 = $("<tr></tr>");
-    var $newtd1;
-    $newtd1 = $('<td>', {
-      "class": "itemname"
-    });
-    $newtd1.text("".itemName);
-    $newtr1.append($newtd1);
-
-    $newtd1 = $('<td>', {
-      "class": "sellername"
-    });
-    $newtd1.text("".sellerName);
-    $newtr1.append($newtd1);
-
-    $newtd1 = $('<td>', {
-      "class": "itemcategory"
-    });
-    $newtd1.text("".itemCat);
-    $newtr1.append($newtd1);
-
-    $newtd1 = $('<td>', {
-      "class": "expirationdate"
-    });
-    $newtd1.text("".itemExp);
-    $newtr1.append($newtd1);
-    // -----------------------
-    //
-    var $newDesc = $("<div><p></p></div>", id: itemID, "class": "itemdescription");
-    $newDesc.text("".itemDesc);
-    $newDesc.hide();
-    $("allitems").append($newtr1);
-    $("itemsTable").after($newDesc);
-  }
 
 });
