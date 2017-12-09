e97d15bb00029ce0c7524173b63903caaea64c8d
haagen17 <33736132+haagen17@users.noreply.github.com>
2017-12-08 21:51:21 -0500

    Add files via upload

--- itemlist.php
+++ itemlist.php
@@ -6,13 +6,15 @@ $database = "sjs4025db";
 
 $conn = new mysqli($server, $username, $password, $database);
            if (!$conn) {
+               echo "connection died";
                die(mysql_error());
            }
            $mysqli->select_db("sjs4025db");
+           if(isset($_POST['itemauto'])){ echo "search item successfully set"; $search_item = $_POST['itemauto']; }
            $results = mysql_query("SELECT I.name as itemname, C.name as categoryname, MAX(B.bid_price) as bidprice , I.exp_date s expiration
            FROM Bid B, Category C, Item I, User U
-           WHERE C.id = I.category_id AND B.item_id = I.id
-           GROUP BY I.id
+           WHERE C.id = I.category_id AND B.item_id = I.id AND I.name LIKE %" .$search_item."$"
+           ." GROUP BY I.id
            ORDER BY COUNT(B.bid_price)
            LIMIT 10");
            while ($row = mysql_fetch_array($results)) {
