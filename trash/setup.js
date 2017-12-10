bb7024cf3bd6b71de6fd8dc92d7623f1cb58ff5e
haagen17 <33736132+haagen17@users.noreply.github.com>
2017-12-10 13:20:11 -0500

    Delete setup.js

--- trash/setup.js
+++
@@ -1,4 +0,0 @@
-auctionItem.all.push(new auctionItem('apple', 1, 'fruit', 4));
-auctionItem.all.push(new auctionItem('banana', 1, 'fruit', 4));
-auctionItem.all.push(new auctionItem('desk', 1, 'furniture', 4));
-auctionItem.all.push(new auctionItem('chair', 1, 'furniture', 4));
